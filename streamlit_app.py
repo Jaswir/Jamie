152bd117e5ba42ac97ab351c632ef42f8906ed65
Jaswir Raghoe <jaswirraghoe@gmail.com>
2023-12-23 21:00:23 +0100

    Security issue Google Credentials fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,21 +28,37 @@ from io import BytesIO
 
 language = "en"
 
-gsc_credentials_json_data = {
-    "type": "service_account",
-    "project_id": "gen-lang-client-0866265611",
-    "private_key_id": "741ca6156f2b6c23ed59119ea7b345d709152b16",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC47+j5ZCRImOd1\nP2kwZdR6HBCLvxCJFB41a7UxDr0KKPbZMO03T9jrb63CblBjKyfeTf7WLZBVtlmo\nhRjvw+W6BHh764ApctIiyJcnhDSmloTCrVIT0nwDGYiicZ33wdac1QDylSQs/mp8\neIfO38DgwOaCyCEFokYj+Kt65LIl6touAPjS6J8aqmrFtNEiwtNFRmo6Od5l7aZI\nS11wQk9zbEEqPE4kornPJdVcIL+isvG+CYEWxzYzJBxYRu5viMeA9aKV5LerW3Vz\nZQ9Ohu23S3RTOxQJcioHvIksCDgUfKEVl/e0tCTDRvp193PViAyL+P3Fh00BxqhC\nlyd3Eg8/AgMBAAECggEAWXwEb7/r6pDJ1irDnB55rEguHWBrANkSMOkX+E/YWrUt\nlDQfZBcnr+wO2LiF/yYfmWqF915+q2qbgXY9da6FVvfY0KVIE0YytY/3y7AlxcGp\nJYW15E+BoAJX4bo2JG7PiH1sgvR1V6wXHHk5xXjxHA9aukdZ6x7072zglItaRAvH\np3PCIOSE+/tTPksSBPelL5mLYVssuv9sUPg1MvcHrvUP/dxKZikXt+BjhS+gUFrD\n61/AbZA+D9PH0ncUJwfGHp/+j0OMgB/FEfVrmFLF7Xr7z6WUsHW2FZj5mp2fOkD6\ndRiMl6F1n2EMuwixmGnVxK2yfTpZyusICyrARxZ56QKBgQDhomVqMvsDDCyDK/Kv\n+wcSB7t/gYvU0mXGRv7gjii9ZAvarWBfGjcx0rDkGV5qo3lrKtHwET9Z7qfc/7Gy\n0YFnJkOEc05wp3l9BHrFItPPXvHWdaHWWIIlL8ZeAtlvmz3ov+UmP7a3Mew0nhUU\nqVtCLvnOMRKHmhbI5nYMYvtPHQKBgQDR02eekk/fwIFJEWNiIj0Mn4zIKX6HE4qo\nWvfqKB1HYsFv5r/KHJBgfmXBC3NPc3dD3ANcT6BjkaOvuhzP+OXnpytlQV4+yI+P\nhRqJewNWRa2a11z3R2fAfN/enepJJhFzE6E4xcqRDr7iUudlZ2sm+LYd2jcmgwoY\ne3AOI479CwKBgBXT5AO2EtpknxnBcqTbn/96iAT5v1FDWV0pGRcYKZDI0OzCwQPy\nQUysmTg3zA586g4YAl87C+njUJHsmoA2DFKyPO3fScNUMEiL2mSfzeXNxY99qb5W\nqaB3UrVdiTNmzUJq7fwZRc/LIJKmiFuLVqy8ZqYv0R8VJQciZGEmYjFxAoGAc0F7\ntpfJ9i9JFPjPLZV/EVMGwuBAFzR9wrKNYXr9cjvl43DNwthj2g00GZEPmwHi7kyJ\nMAixImBamGlT8E1ZBCFIbgs1GKmUahKEHCaNE7Ocg+RutYnsjMhLNH5rthiKFzse\nIRBMj9eYtLszTfk726FccBNwnd/3CLGtQKb5oTcCgYEA4LGQ2xPw7Ae5wEqkusGB\nYJZR1yxtjK4jHlg687MwhTWSd2k0KfsZRHQOYzEN3a8eVHPrMphiyWCwaCFRy7Vg\nNHwAcTtDUjs/JBzfEcgIEiYr8kjCIJy0gZj96qsPyNGoyM6/HUYKcVuQiw0HmiEy\nXF7CxQy8xljcxnW+H2ZVkmI=\n-----END PRIVATE KEY-----\n",
-    "client_email": "jamieserviceaccount@gen-lang-client-0866265611.iam.gserviceaccount.com",
-    "client_id": "110334946025857142107",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/jamieserviceaccount%40gen-lang-client-0866265611.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com",
+GSC_CREDS_TYPE = environ.get("GSC_CREDS_TYPE")
+GSC_CREDS_TYPE_PROJECT_ID = environ.get("GSC_CREDS_TYPE_PROJECT_ID")
+GSC_CREDS_PRIVATE_KEY_ID = environ.get("GSC_CREDS_PRIVATE_KEY_ID")
+GSC_CREDS_PRIVATE_KEY = environ.get("GSC_CREDS_PRIVATE_KEY").replace("\\n", "\n")
+GSC_CREDS_CLIENT_EMAIL = environ.get("GSC_CREDS_CLIENT_EMAIL")
+GSC_CREDS_CLIENT_ID = environ.get("GSC_CREDS_CLIENT_ID")
+GSC_CREDS_CLIENT_X509_CERT_URL = environ.get("GSC_CREDS_CLIENT_X509_CERT_URL")
+
+gsc_credentials_dict = {
+  "type": GSC_CREDS_TYPE,
+  "project_id": GSC_CREDS_TYPE_PROJECT_ID,
+  "private_key_id": GSC_CREDS_PRIVATE_KEY_ID,
+  "private_key": GSC_CREDS_PRIVATE_KEY,
+  "client_email": GSC_CREDS_CLIENT_EMAIL,
+  "client_id": GSC_CREDS_CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": GSC_CREDS_CLIENT_X509_CERT_URL,
+  "universe_domain": "googleapis.com"
 }
 
-environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(gsc_credentials_json_data)
+
+
+json_data = json.dumps(gsc_credentials_dict, indent=4)
+json_bytes = json_data.encode('utf-8')
+
+with open("gsc_creds.json", mode="wb") as f:
+    f.write(json_bytes)
+
+environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'gsc_creds.json'
 
 uploaded_file = st.file_uploader("Choose an image file to upload", type="jpg")
 if uploaded_file is not None:
